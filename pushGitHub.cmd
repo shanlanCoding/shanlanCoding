1731752e6569da759f6670e3d0533810ea6bb0d4
山岚 <misterchou@qq.com>
2019-06-10 01:48:16 +0800

    添加菜单

--- pushGitHub.cmd
+++ pushGitHub.cmd
@@ -3,6 +3,6 @@ set /p commit=commit:
 title auto commit
 D:\Git\bin\git.exe add -A
 D:\Git\bin\git.exe commit -m %commit%
-D:\Git\bin\git.exe push origin master:HEXO
+D:\Git\bin\git.exe push origin
 pause >null
 :exit
