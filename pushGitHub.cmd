d1d1dcef051f22a1942e508580ea7b72178bc60a
山岚 <misterchou@qq.com>
2019-06-10 01:32:31 +0800

    添加菜单和脚本

--- pushGitHub.cmd
+++ pushGitHub.cmd
@@ -3,5 +3,5 @@ set /p commit=commit:
 title auto commit
 D:\Git\bin\git.exe add -A
 D:\Git\bin\git.exe commit -m %commit%
-D:\Git\bin\git.exe push origin HEXO:bfinger
+D:\Git\bin\git.exe push origin master:HEXO
 exit
