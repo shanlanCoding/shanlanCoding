93644ecb5e7fbacef41482dc1564896fcb358a72
shanlanCoding <291637732@qq.com>
2019-08-18 14:00:35 +0800

    添加文章

--- pushGitHub-Hasee.cmd
+++ pushGitHub-Hasee.cmd
@@ -1,9 +1,9 @@
 @echo off
 set /p commit=commit:
 title auto commit
-D:\Git\git-bash.exe add -A
-D:\Git\git-bash.exe commit -m %commit%
-D:\Git\git-bash.exe push
+D:\Git\bin\git.exe add -A
+D:\Git\bin\git.exe commit -m %commit%
+D:\Git\bin\git.exe push
 echo 命令执行完毕，请按任意键关闭
 pause >null
 :exit
