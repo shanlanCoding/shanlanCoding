dbd7c184fa495220d5e2f5556bdb2414dc0ec298
shanLan <291637732@qq.com>
2019-08-19 15:31:06 +0800

    备份2

--- deployHexo-TopLink.cmd
+++ deployHexo-TopLink.cmd
@@ -1,4 +1,9 @@
 @echo off
 E:
 cd E:\HEXO\shanlancoding.github.io
-hexo g && hexo d
+hexo g -d && mshta vbscript:msgbox("部署完成，准备push源程序到仓库",6,"部署完成")(window.close) && call pushGitHub-TopLink.cmd
+
+;;color 4
+;;echo 部署完成后，push源程序到仓库
+::部署完成后，push源程序到仓库
+;;call pushGitHub-TopLink.cmd
