9678f0dd375cd6ca0bf11f90303c3b83e2493042
shanLan <291637732@qq.com>
2019-08-19 15:49:39 +0800

    更新批处理，让部署推送更简单

--- deployHexo-Hasee.cmd
+++ deployHexo-Hasee.cmd
@@ -2,4 +2,5 @@
 ::call "D:\GitHub_Pages\Hexo-git\shanlancoding.github.io\pushGitHub.cmd"
 D:
 cd D:\GitHub_Pages\Hexo-git\shanlancoding.github.io
-hexo g && hexo d
+hexo g -d && mshta vbscript:msgbox("部署完成，准备push源程序到仓库",6,"部署完成")(window.close) && color 4 && call pushGitHub-Hasee.cmd
+
--- deployHexo-TopLink.cmd
+++ deployHexo-TopLink.cmd
@@ -1,9 +1,5 @@
 @echo off
 E:
 cd E:\HEXO\shanlancoding.github.io
-hexo g -d && mshta vbscript:msgbox("部署完成，准备push源程序到仓库",6,"部署完成")(window.close) && call pushGitHub-TopLink.cmd
+hexo g -d && mshta vbscript:msgbox("部署完成，准备push源程序到仓库",6,"部署完成")(window.close) && color 4 && call pushGitHub-TopLink.cmd
 
-;;color 4
-;;echo 部署完成后，push源程序到仓库
-::部署完成后，push源程序到仓库
-;;call pushGitHub-TopLink.cmd
